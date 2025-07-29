cd4c26e2c8c8da5c59278578e54ad164ec05665a
Mohammad Faizaan <113120627+FaizaanMd@users.noreply.github.com>
2025-07-29 21:41:59 +0530

    Add .next/static/chunks/src_app_page_tsx_0ef0cac3._.js 1753805514095

---
+++ .next/static/chunks/src_app_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d370a291._.js",
+  "static/chunks/src_48f4c032._.js"
+],
+    source: "dynamic"
+});
